b13ce73b0d01e41cfa88dc9e80b40d6e1bb32dc0
JGuder15 <93221338+JGuder15@users.noreply.github.com>
2021-11-01 12:11:40 -0500

    Uploaded Out-Documents
    Added compiled products from the DITA files.

---
+++ webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,30 @@
+define(function() {var keywords=[{w:"My",p:["p0","p1"]},{w:"Home",p:["p0"]},{w:"Life",p:["p0","p1"]},{w:"Work",p:["p1","p20","p22"]},{w:"Dr.",p:["p2","p4"]},{w:"Jo",p:["p2"]},{w:"Mackiewicz",p:["p2"]},{w:"Joan",p:["p3"]},{w:"Chamberlin",p:["p3"]},{w:"Joseph",p:["p4"]},{w:"Cheatle",p:["p4"]},{w:"Iowa",p:["p5","p13"]},{w:"State",p:["p5","p13"]},{w:"University,",p:["p5"]},{w:"Ames,",p:["p5"]},{w:"IA",p:["p5"]},{w:"Rosemount",p:["p6"]},{w:"High",p:["p6"]},{w:"School,",p:["p6"]},{w:"Rosemount,",p:["p6"]},{w:"MN",p:["p6"]},{w:"Ellen",p:["p7"]},{w:"M.",p:["p7"]},{w:"Walvoord",p:["p7"]},{w:"Scholarship",p:["p7"]},{w:"Adobe",p:["p8"]},{w:"InDesign",p:["p8"]},{w:"Microsoft",p:["p9"]},{w:"365",p:["p9"]},{w:"Suite",p:["p9"]},{w:"Basic",p:["p10"]},{w:"HTML",p:["p10"]},{w:"and",p:["p10","p11","p21"]},{w:"CSS",p:["p10"]},{w:"Assistant",p:["p11"]},{w:"to",p:["p11"]},{w:"the",p:["p11"]},{w:"Rhetoric",p:["p11"]},{w:"Professional",p:["p11"]},{w:"Communication",p:["p11","p13"]},{w:"Coordinator",p:["p11"]},{w:"IEOP",p:["p12"]},{w:"Student",p:["p12"]},{w:"Tutor",p:["p12"]},{w:"Consultant",p:["p13"]},{w:"at",p:["p13"]},{w:"University",p:["p13"]},{w:"Jet-Black",p:["p14"]},{w:"Data",p:["p14"]},{w:"Entry",p:["p14"]},{w:"Clerk",p:["p14"]},{w:"about",p:["p15","p16"]},{w:"Me",p:["p16"]},{w:"education",p:["p17"]},{w:"reference",p:["p18"]},{w:"References",p:["p19"]},{w:"Skills",p:["p21"]},{w:"Accolades",p:["p21"]},{w:"Experience",p:["p22"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[0, 3, 2];
+ph["p2"]=[4, 5, 6];
+ph["p3"]=[7, 8];
+ph["p4"]=[4, 9, 10];
+ph["p5"]=[11, 12, 13, 14, 15];
+ph["p6"]=[16, 17, 18, 19, 20];
+ph["p7"]=[21, 22, 23, 24];
+ph["p8"]=[25, 26];
+ph["p9"]=[27, 28, 29];
+ph["p10"]=[30, 31, 32, 33];
+ph["p21"]=[56, 32, 57];
+ph["p20"]=[3];
+ph["p12"]=[41, 42, 43];
+ph["p11"]=[34, 35, 36, 37, 32, 38, 39, 40];
+ph["p22"]=[3, 58];
+ph["p14"]=[47, 48, 49, 50];
+ph["p13"]=[39, 44, 45, 11, 12, 46];
+ph["p16"]=[51, 52];
+ph["p15"]=[51];
+ph["p18"]=[54];
+ph["p17"]=[53];
+ph["p19"]=[55];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {1:15,10:18,4:17,15:-1,11:19,12:19,3:17,16:-1,17:-1,6:16,8:18,5:16,19:-1,18:-1,0:15,2:17,9:18,14:19,7:18,13:19};
+});
